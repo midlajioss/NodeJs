8a2f91d405e12eeb270806d9df011686c3c97a6e
midlaj <midlaj@teamioss.in>
2025-03-29 10:51:19 +0530

    Railway Connected

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://neondb_owner:npg_WnfVhqXpM75m@ep-odd-truth-a5oxw2m9-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require',
+  connectionString: 'mysql://root:QFFoThCbdGOrPnjYjYlCsJjhMAHmcmIj@mainline.proxy.rlwy.net:11335/railway',
   ssl: {
     rejectUnauthorized: false, // Required for Neon
   }
 });
 
 pool.connect()
-  .then(() => console.log('Connected to PostgreSQL on Neon.tech'))
+  .then(() => console.log('Connected to Railway DB'))
   .catch(err => console.error('Connection error:', err));
 
 module.exports = pool;
